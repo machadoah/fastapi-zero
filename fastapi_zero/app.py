95f1af2bfda56be0e549cbfd021d9d52cf295249
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-01-29 21:57:28 -0300

    ✨ Adicionando código de status e modelo de resposta para read_root

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=status.HTTP_200_OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, World!'}
