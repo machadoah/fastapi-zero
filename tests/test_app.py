f817cb3d16d05aad48c165c730cd7ddcee4df74c
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-01-29 21:58:17 -0300

    🧪 Adicionando teste para endpoint HTML

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,3 +13,13 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     assert response.json() == {
         'message': 'Hello, World!'
     }  # assert (afirmação)
+
+
+def test_read_html_deve_retornar_ok_e_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == status.HTTP_200_OK
+    assert '<h1>Olá, Mundo!</h1>' in response.text
+
