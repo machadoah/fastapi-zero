8e82ef9e8098421ec7ad2c3762888b48c53f0b92
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-01-29 21:57:47 -0300

    ✨ Adicionando endpoint para retornar HTML

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -9,3 +9,17 @@ app = FastAPI()
 @app.get('/', status_code=status.HTTP_200_OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, World!'}
+
+
+@app.get('/html', status_code=status.HTTP_200_OK, response_class=HTMLResponse)
+def read_html():
+    return """
+<html>
+    <head>
+        <title>Nosso olá mundo</title>
+    </head>
+    <body>
+        <h1>Olá, Mundo!</h1>
+    </body>
+</html>
+"""
