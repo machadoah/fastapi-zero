ab1a3b9d648140155dbc105e91299d13a16ff855
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-01-29 23:59:33 -0300

    ✨ Implementação das operações CRUD para usuários na API

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import HTMLResponse
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=status.HTTP_200_OK, response_model=Message)
@@ -23,3 +31,53 @@ def read_html():
     </body>
 </html>
 """
+
+
+@app.post(
+    '/users/', status_code=status.HTTP_201_CREATED, response_model=UserPublic
+)
+def crete_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user_by_id(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'User with id {user_id} not found!',
+        )
+    return database[user_id - 1]
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='User not found!'
+        )
+
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='User not found!'
+        )
+    del database[user_id - 1]
+
+    return {'message': f'User with id {user_id} deleted!'}
