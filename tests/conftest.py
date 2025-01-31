5c44468ab961ef6e18f4599af1d5d89a5c162bc6
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-01-30 22:22:14 -0300

    🧪 Adicionando fixture de sessão para testes com banco de dados em memória

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,27 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from fastapi_zero.app import app
+from fastapi_zero.models import table_registry
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    # Etapa de Setup
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    # gerenciamento de contexto
+    with Session(engine) as session:
+        yield session
+        # execução até aqui
+
+    # Etapa de Teardown
+    table_registry.metadata.drop_all(engine)
