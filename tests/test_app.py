869b127c777d7319c040e2e287532abee8367f29
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-02 13:17:35 -0300

    ♻️ Simplificando asserção no teste de leitura da raiz para melhor legibilidade

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,4 @@ def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {
-        'message': 'Hello, World!'
-    }
+    assert response.json() == {'message': 'Hello, World!'}
