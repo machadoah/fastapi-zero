71d6ad788ca37812c3af907b9889645d005ff044
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-02 12:07:31 -0300

    ♻️ Adicionando router para a rota `root`

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from fastapi_zero.routers import auth, users
+from fastapi_zero.routers import auth, root, users
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(root.router)
