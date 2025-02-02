03bce7300cd9b0022bd283f9200998166028b04d
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-02 11:55:37 -0300

    ♻️ Criando router para a rota `/users`

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -9,14 +9,9 @@ from fastapi_zero.models import User
 from fastapi_zero.schemas import (
     Message,
     Token,
-    UserList,
-    UserPublic,
-    UserSchema,
 )
 from fastapi_zero.security import (
     create_access_token,
-    get_current_user,
-    get_password_hash,
     verify_password,
 )
 
@@ -40,112 +35,6 @@ def read_html():
     </body>
 </html>
 """
-
-
-@app.post(
-    '/users/', status_code=status.HTTP_201_CREATED, response_model=UserPublic
-)
-def crete_user(user: UserSchema, session: Session = Depends(get_session)):
-    db_user = session.scalar(
-        select(User).where(
-            (User.username == user.username) | (User.email == user.email)
-        )
-    )
-
-    if db_user:
-        if db_user.username == user.username:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Username already exists.',
-            )
-
-        if db_user.email == user.email:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Email already exists.',
-            )
-
-    db_user = User(
-        username=user.username,
-        password=get_password_hash(user.password),
-        email=user.email,
-    )
-
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
-
-    return db_user
-
-
-@app.get('/users/', response_model=UserList)
-def read_users(
-    limit: int = 10,
-    skip: int = 0,
-    session: Session = Depends(get_session),
-):
-    users = session.scalars(select(User).limit(limit).offset(skip))
-    return {'users': users}
-
-
-@app.get('/users/{user_id}', response_model=UserPublic)
-def read_user_by_id(user_id: int, session: Session = Depends(get_session)):
-    if user_id < 0:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='ID must be a positive integer',
-        )
-
-    user = session.scalar(select(User).where(User.id == user_id))
-
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'User with id {user_id} not found!',
-        )
-
-    return user
-
-
-@app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(
-    user_id: int,
-    user: UserSchema,
-    session: Session = Depends(get_session),
-    current_user=Depends(get_current_user),
-):
-    if current_user.id != user_id:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='No enough permissions',
-        )
-
-    current_user.username = user.username
-    current_user.password = get_password_hash(user.password)
-    current_user.email = user.email
-
-    session.commit()
-    session.refresh(current_user)
-
-    return current_user
-
-
-@app.delete('/users/{user_id}', response_model=Message)
-def delete_user(
-    user_id: int,
-    session: Session = Depends(get_session),
-    current_user=Depends(get_current_user),
-):
-    if current_user.id != user_id:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='No enough permissions',
-        )
-
-    session.delete(current_user)
-    session.commit()
-
-    return {'message': f'User with id {user_id} deleted!'}
 
 
 @app.post('/token', response_model=Token)
