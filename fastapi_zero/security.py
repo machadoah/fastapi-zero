11d82c353491d6a3cb6a7249f8abedc2c6c05252
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-02 01:58:30 -0300

    🔒️ Implementando criação e validação de tokens de acesso com JWT

--- fastapi_zero/security.py
+++ fastapi_zero/security.py
@@ -1,6 +1,23 @@
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt import DecodeError, decode, encode
 from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from fastapi_zero.database import get_session
+from fastapi_zero.models import User
+from fastapi_zero.schemas import TokenData
 
 pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+SECRET_KEY = 'your-secret-key'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def get_password_hash(password: str):
@@ -9,3 +26,47 @@ def get_password_hash(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(password=plain_password, hash=hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    # adiciona um tempo de 30 minutos para a expiração
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({'exp': expire})
+
+    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+
+def get_current_user(
+    session: Session = Depends(get_session),
+    token: str = Depends(oauth2_scheme),
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+
+    try:
+        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if not username:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except DecodeError:
+        raise credentials_exception
+
+    user = session.scalar(
+        select(User).where(User.email == token_data.username)
+    )
+
+    if not user:
+        raise credentials_exception
+
+    return user
