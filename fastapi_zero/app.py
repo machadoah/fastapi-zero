8c2b6bbb963fc271ede70a41925386ab2e7c9595
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-02 12:06:54 -0300

    ♻️ Criando router para a rota `/`

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,31 +1,8 @@
-from fastapi import FastAPI, status
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 
 from fastapi_zero.routers import auth, users
-from fastapi_zero.schemas import (
-    Message,
-)
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(users.router)
-
-
-@app.get('/', status_code=status.HTTP_200_OK, response_model=Message)
-def read_root():
-    return {'message': 'Hello, World!'}
-
-
-@app.get('/html', status_code=status.HTTP_200_OK, response_class=HTMLResponse)
-def read_html():
-    return """
-<html>
-    <head>
-        <title>Nosso olá mundo</title>
-    </head>
-    <body>
-        <h1>Olá, Mundo!</h1>
-    </body>
-</html>
-"""
