8b52fa695fd2af162acae15460f7a3810f93c206
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-01 22:41:33 -0300

    🧪 Refatorando fixtures de teste para incluir gerenciamento de sessão e criação de usuário

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,33 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from fastapi_zero.app import app
-from fastapi_zero.models import table_registry
+from fastapi_zero.database import get_session
+from fastapi_zero.models import User, table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
     # Etapa de Setup
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     # gerenciamento de contexto
@@ -25,3 +38,13 @@ def session():
 
     # Etapa de Teardown
     table_registry.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Teste', email='teste@test.com', password='testtest')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
