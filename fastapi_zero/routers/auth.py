6d956503c54685322ac091145e4936f0ca455a94
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-02 19:47:27 -0300

    🔧 Adicionando comentários para esclarecer a lógica de autenticação no endpoint de criação de token

--- fastapi_zero/routers/auth.py
+++ fastapi_zero/routers/auth.py
@@ -16,25 +16,23 @@ T_Session = Annotated[Session, Depends(get_session)]
 T_OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
 
 
+# endpoint para criar token
 @router.post('/token', response_model=Token)
 def login_for_access_token(
-    session: T_Session,
-    form_data: T_OAuth2Form,
+    session: T_Session,  # session, conexão com banco de dados
+    form_data: T_OAuth2Form,  # formulários oauth2
 ):
+    # com base no username do formulário, vê se ele existe no banco
     user = session.scalar(select(User).where(User.email == form_data.username))
 
-    if not user:
+    # se o usuário não existir ou a senha não for válida
+    if not user or not verify_password(form_data.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail='Incorrect email or password',
         )
 
-    if not verify_password(form_data.password, user.password):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Incorrect email or password',
-        )
-
+    # cria o access token
     access_token = create_access_token(data={'sub': user.email})
 
     return {'access_token': access_token, 'token_type': 'Bearer'}
