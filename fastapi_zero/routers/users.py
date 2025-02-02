cb616910b5bdec8e8b0c101ce6c48028669ec52f
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-02 13:58:17 -0300

    🚨 Alterando código de status para proibição de acesso em atualizações e exclusões de usuário

--- fastapi_zero/routers/users.py
+++ fastapi_zero/routers/users.py
@@ -88,7 +88,7 @@ def update_user(
 ):
     if current_user.id != user_id:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_403_FORBIDDEN,
             detail='No enough permissions',
         )
 
@@ -110,7 +110,7 @@ def delete_user(
 ):
     if current_user.id != user_id:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_403_FORBIDDEN,
             detail='No enough permissions',
         )
 
