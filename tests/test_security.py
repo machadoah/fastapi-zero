6221463b17f7394d0c01fb2270e6f74061391ab5
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-02 13:15:27 -0300

    ♻️ Adicionando teste para validação de token JWT inválido no endpoint de exclusão de usuário

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,3 +1,4 @@
+from fastapi import status
 from jwt import decode
 
 from fastapi_zero.security import ALGORITHM, SECRET_KEY, create_access_token
@@ -11,3 +12,12 @@ def test_jwt():
 
     assert result['sub'] == data['sub']
     assert result['exp']
+
+
+def test_jwt_invalid_token(client):
+    response = client.delete(
+        '/users/1', headers={'Authorization': 'Bearer token-invalido'}
+    )
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
