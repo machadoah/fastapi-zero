0ccab8061644a08eed3195aec81bb83008b3dd3f
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-02 21:57:44 -0300

    🔧 Adicionando teste para verificar expiração de token usando freezegun

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,4 +1,5 @@
 from fastapi import status
+from freezegun import freeze_time
 
 
 def test_get_token(client, user):
@@ -12,3 +13,25 @@ def test_get_token(client, user):
     assert response.status_code == status.HTTP_200_OK
     assert token['token_type'] == 'Bearer'
     assert 'access_token' in token
+
+
+def test_token_expired_after_time(client, user):
+    with freeze_time('2004-05-27 15:30:00'):
+        # Gerar o token
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+
+    assert response.status_code == status.HTTP_200_OK
+    token = response.json()['access_token']
+
+    with freeze_time('2004-05-27 16:01:00'):
+        # Consumir a rota com o token expirado
+        response = client.delete(
+            f'/users/{user.id}',
+            headers={'Authorization': f'Bearer {token}'},
+        )
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.json() == {'detail': 'Could not validate credentials'}
