b41404d6f7a64760c636a812e59f149c4025fd95
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-03 07:15:14 -0300

    ✨ Adicionando o roteador de tarefas ao aplicativo FastAPI

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, status
 
-from fastapi_zero.routers import auth, html, users
+from fastapi_zero.routers import auth, html, todo, users
 from fastapi_zero.schemas import Message
 
 app = FastAPI()
@@ -8,6 +8,7 @@ app = FastAPI()
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(html.router)
+app.include_router(todo.router)
 
 
 @app.get('/', status_code=status.HTTP_200_OK, response_model=Message)
