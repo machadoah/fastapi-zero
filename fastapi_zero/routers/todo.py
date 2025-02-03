35fd959a17a55f80766338751bc2f6108fcb7b13
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-02-03 07:29:06 -0300

    ✨ Reorganizando importações no roteador de tarefas para melhor legibilidade

--- fastapi_zero/routers/todo.py
+++ fastapi_zero/routers/todo.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from fastapi_zero.database import get_session
-from fastapi_zero.models import User, Todo
+from fastapi_zero.models import Todo, User
 from fastapi_zero.schemas import TodoPublic, TodoSchema
 from fastapi_zero.security import get_current_user
 
@@ -22,9 +22,9 @@ def create_todo(todo: TodoSchema, user: T_CurrentUser, session: T_Session):
         state=todo.state,
         user_id=user.id,
     )
-    
+
     session.add(db_todo)
     session.commit()
     session.refresh(db_todo)
-    
+
     return db_todo
